e1d1f212c830bc6102002eaa109bd1e8912cc121
Jayden <jumpers.jaeheung@gmail.com>
2017-03-22 22:56:03 +0900

    Add files via upload

---
+++ python_basic_02.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 22 22:16:35 2017
+
+@author: jayden.yoo
+"""
+
+# 파이썬으로 글자 인쇄하기
+print("Hello")
+print('Good day!')
+print("한글로도 쓸 수 있어요.")
+
+# 문자열 연산
+print("Hello, " + "World!")
+print("내 이름은 " + "홍길도" + "입니다.")
+print("*" * 10)
+
+# 숫자를 문자열로 바꾸기
+str(7)
+
+n = 10
+print("별표를 " + str(n) + "번 인쇄합니다.")
+print("*" * n)
+
+# 한 줄 띄우기
+print("한 줄 쓰고\n그 다음 줄을 쓴다.")
+
+# 줄을 바꾸지 않고 이어서 인쇄하기
+print("한 줄 쓰고 ", end="")
+print("이어서 쓴다.")
+
+# 문자열 값을 가지는 변수
+name = "홍길동"
+print("내 이름은 " + name + "입니다.")
+
+mark = "$"
+n = 20
+print(mark + " 기호를 " + str(n) + "번 인쇄합니다.")
+print(mark * n)
+
+# 따옴표를 인쇄하기
+print('둘리가 "호이!"하고 말했어요.')
+print("둘리가 '이제 어디로 가지?'하고 생각했어요.")
+
+# Q1. 다음 문장을 인쇄해 본다.
+
+# Didn't even really wanna go
+# But if you get me out, you get a show
+# There's so many bodies on the floor
+# So baby we should go and add some more
+# Are you down, d-d-down, d-d-down, d-d-down, down, down?
+# Down, d-d-down, d-d-down, d-d-down, down, down?
+
+# Q2. 문자열의 덧셈과 곱셈 연산을 사용하여 다음 문자열을 인쇄한다.
+
+# @@@@@@
+# @    @
+# @    @
+# @    @
+# @@@@@@
